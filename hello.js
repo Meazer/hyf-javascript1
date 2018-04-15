4173232eabf82ea0bf05ec44cd0a293974e96f47
Meazer <meazer.m@gmail.com>
2018-04-15 15:46:53 +0200

    initial commit

---
+++ hello.js
@@ -0,0 +1,29 @@
+console.log("Hello world");
+
+const myName = "Meazer";
+let age = 33;
+age = 34;
+console.log(myName, age );
+
+const isMale = true;
+
+if (isMale) {
+    console.log("Male");
+}else { console.log("female"); }
+
+const months = ["January","February","March"];
+let i;
+for (i=0;i<months.length;i++){
+    console.log(months[i]); 
+
+}
+console.log(months); 
+let hyf;
+hyf = 1;
+console.log("My name is",myName);
+
+const hello = "hello world!";
+console.log(hello.slice(0,5));
+const start = hello.startsWith("He");
+console.log(start);
+
